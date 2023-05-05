9f940a7d7f1c36ad39905664fa4efde730329b18
sasidharreddysai <sasidharreddysai@gmail.com>
2023-05-05 16:02:01 +0530

    AI Summarizer

---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import { logo } from "../assets";
+
+const Hero = () => {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz_logo" className="w-28 object-contain" />
+
+        <button type="button" onClick={() => window.open()} className="black_btn">
+          GitHub
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Summarie Articles with <br className="max-md:hidden" />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with summize, an open-source artice summarizer that transforms lengthy articles into clear
+        and concise summaries
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
